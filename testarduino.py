b2f1f7663aa3e401873550ad2e0e5682c387a65f
niclasflach <niclas@flacken.com>
2023-11-12 19:08:59 +0100

    python scripts added

--- testarduino.py
+++ testarduino.py
@@ -3,6 +3,8 @@
 import serial
 import time
 import keyboard
+import pygame
+
 
 
 arduino = serial.Serial(port="/dev/ttyUSB0", baudrate=9600, timeout=.1)
@@ -12,7 +14,7 @@ b=360
 c=400
 d=400
 e=400
-speed = 20
+speed = 5
 
 
 
@@ -23,35 +25,40 @@ def changePos(a,b,c,d,e):
     data = arduino.readline()
 
     return data
+pygame.init()
+
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Physics")
+white = (255, 255, 255)
 
 while True:
     changePos(a,b,c,d,e)
-    time.sleep(0.5)
-    if keyboard.is_pressed('a'):
-        a += speed
-        print(f"A:{a}")
-    if keyboard.is_pressed('z'):
-        a -= speed
-        print(f"A:{a}")
-    if keyboard.is_pressed('s'):
-        b += speed
-        print(f"B:{b}")
-    if keyboard.is_pressed('x'):
-        b -= speed
-        print(f"B:{b}")
-    if keyboard.is_pressed('d'):
-        c += speed
-    if keyboard.is_pressed('c'):
-        c -= speed
-    if keyboard.is_pressed('f'):
-        d += speed
-    if keyboard.is_pressed('v'):
-        d -= speed
-    if keyboard.is_pressed('g'):
-        e += speed
-    if keyboard.is_pressed('b'):
-        e -= speed
-            
-    
+    time.sleep(0.1)
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_DOWN:
+                a += speed
+                print(f"A:{a}")
+            if event.key == pygame.K_UP:
+                a -= speed
+                print(f"A:{a}")
+            if event.key == pygame.K_RIGHT:
+                b += speed
+                print(f"B:{b}")
+            if event.key == pygame.K_LEFT:
+                b -= speed
+                print(f"B:{b}")
+            if event.key == pygame.K_RCTRL:
+                a=400
+                b=360
+                c=400
+                d=400
+                e=400            
+    screen.fill(white)
+    pygame.display.flip()
 
 
